c73ab4cfe8e026fbf52e995a11d6b18f1db08cef
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-11 11:07:58 -0600

    logo and h1 styled

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <img src="../assets/g-dino.png" alt=""> <h1>Galvanize Reads</h1>
+        <img src="../assets/logo.png" alt=""> <h1>Galvanize Reads</h1>
     </header>
 </template>
 
@@ -11,5 +11,23 @@ export default {
 </script>
 
 <style>
+header {
+    grid-row: 1 / 2;
+    display: grid;
+    grid-template-columns: 25% 50% 15%;
+    grid-template-rows: 50% 50%;
+}
 
+header img {
+    grid-row: 1/3;
+    grid-column: 1/2;
+    height: 100px;
+    width: auto;
+}
+
+header h1 {
+    grid-row: 1/3;
+    grid-column: 1/2;
+    padding: 5px;
+}
 </style>
