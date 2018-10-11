c23d093957ebbde99dc917a36ce2e6497a06225b
Maddie Kreamer <maddiekreamer@gmail.com>
2018-10-11 10:49:12 -0600

    Components added, header logo and title added

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <img src="../assets/g-dino.png" alt=""> <h1>Galvanize Reads</h1>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style>
+
+</style>
